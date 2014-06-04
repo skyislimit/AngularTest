191e0320de94f90c954893c509b745ff9b220bfd
skyislimit <skyislimit@qq.com>
2014-06-05 00:21:29 +0800

    angular tutorial Step 7

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,45 +2,11 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp',[]);
+var phonecatControllers = angular.module('phonecatControllers',[]);
 
-<!--
-phonecatApp.controller('PhoneListCtrl',function($scope, $http){
-    $http.get('phones/phones.json').success(function(data){
-        $scope.phones = data;
-    });
-
-    $scope.orderProp = "age";
-});
--->
-
-<!-- to avoid minification - method 1-->
-<!--
-function PhoneListCtrl($scope, $http){
-    $http.get('phones/phones.json').success(function(data){
-        $scope.phones = data;
-    });
-
-    $scope.orderProp = "age";
-};
-PhoneListCtrl.$inject = ['$scope', '$http'];
-phonecatApp.controller('PhoneListCtrl', PhoneListCtrl);
--->
-
-<!-- to avoid minification - method 2 -->
-<!--
-function PhoneListCtrl($scope, $http){
-    $http.get('phones/phones.json').success(function(data){
-        $scope.phones = data;
-    });
-
-    $scope.orderProp = "age";
-};
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', PhoneListCtrl])
--->
 
 <!-- same as above method 2, except use anonymous function -->
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
     function($scope, $http){
         $http.get('phones/phones.json').success(function(data){
             // $scope.phones = data.splice(0,10);    // get the first 10 records
@@ -48,4 +14,9 @@ phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
         });
 
         $scope.orderProp = "age";
+    }]);
+
+phonecatControllers.controller('PhoneDetailCtrl', ['$routeParmas','$scope',
+    function($scope, $routeParams){
+        $scope.phoneId = $routeParams.phoneId;
     }]);
