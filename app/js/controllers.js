efcb74ef01803b2352a84132c369a9b56e091896
skyislimit <skyislimit@qq.com>
2014-06-05 02:57:09 +0800

    angular tutorial Step 10

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -20,6 +20,15 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope','$routeParams','$htt
     function($scope,$routeParams,$http){
         $http.get('phones/'+ $routeParams.phoneId + '.json').success(function(data){
             $scope.phone = data;
+            $scope.mainImageUrl = data.images[0];
         });
         //$scope.phoneId = $routeParams.phoneId;
+
+        $scope.setImage = function(imageUrl) {
+            $scope.mainImageUrl = imageUrl;
+        };
+
+        $scope.hello = function(name){
+            alert('Hello ' + (name || 'world') + '!');
+        }
     }]);
