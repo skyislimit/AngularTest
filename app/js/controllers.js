9826061419bc8c2e183067831c4abae31e73bcf8
skyislimit <skyislimit@qq.com>
2014-06-04 14:09:53 +0800

    angular tutorial Step 5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,24 +4,48 @@
 
 var phonecatApp = angular.module('phonecatApp',[]);
 
-phonecatApp.controller('PhoneListCtrl',function($scope){
-    $scope.phones = [
-        {
-            'name':'Nexus S',
-            'snippet':'Fast just got faster with Nexus S.',
-            'age':1
-        },
-        {
-            'name':'Motorola XOOM with Wi-Fi',
-            'snippet':'The Next, Next Generation tablet.',
-            'age':2
-        },
-        {
-            'name':'MOTOROLA XOOM',
-            'snippet':'The Next, Next Generation tablet.',
-            'age':3
-        }
-    ];
+<!--
+phonecatApp.controller('PhoneListCtrl',function($scope, $http){
+    $http.get('phones/phones.json').success(function(data){
+        $scope.phones = data;
+    });
 
     $scope.orderProp = "age";
 });
+-->
+
+<!-- to avoid minification - method 1-->
+<!--
+function PhoneListCtrl($scope, $http){
+    $http.get('phones/phones.json').success(function(data){
+        $scope.phones = data;
+    });
+
+    $scope.orderProp = "age";
+};
+PhoneListCtrl.$inject = ['$scope', '$http'];
+phonecatApp.controller('PhoneListCtrl', PhoneListCtrl);
+-->
+
+<!-- to avoid minification - method 2 -->
+<!--
+function PhoneListCtrl($scope, $http){
+    $http.get('phones/phones.json').success(function(data){
+        $scope.phones = data;
+    });
+
+    $scope.orderProp = "age";
+};
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', PhoneListCtrl])
+-->
+
+<!-- same as above method 2, except use anonymous function -->
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+    function($scope, $http){
+        $http.get('phones/phones.json').success(function(data){
+            // $scope.phones = data.splice(0,10);    // get the first 10 records
+            $scope.phones = data;                    // get all records
+        });
+
+        $scope.orderProp = "age";
+    }]);
